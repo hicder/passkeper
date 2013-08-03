65be015a9413fb9b551073205d489dfae49a2479
Duc Hieu Pham <hieup@outlook.com>
2013-08-03 08:57:12 +0700

    Initial Commit

---
+++ PassKeper/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PassKeper
+//
+//  Created by Hieu on 8/3/13.
+//  Copyright (c) 2013 Hieu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ PassKeper/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PassKeper
+//
+//  Created by Hieu on 8/3/13.
+//  Copyright (c) 2013 Hieu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
