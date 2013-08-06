39386a79cad0f836f1b2056c0ff527fe0f91392d
Duc Hieu Pham <hieup@outlook.com>
2013-08-06 12:52:58 +0700

    clean

--- public/db_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('DB_USER', "hieu");
-define('DB_PASSWORD' "");
-define('DB_DATABASE', "passkeeper");
-define('DB_SERVER', "http://www.google.com");
-
-$mysqli = new mysqli(DB_SERVER, DB_USER, DB_PASSWORD, DB_DATABASE);
-?>
